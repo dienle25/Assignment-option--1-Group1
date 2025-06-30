ec833ce532cd0e8ef0c7b8dfbdf232e176bd3b7b
PhongNguyenNhatLam <LamPNNCE200024@gmail.com>
2025-06-30 22:43:59 +0700

    haiz

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def load_data(file_path):
         with open(file_path, 'w', encoding='utf-8') as f:
             json.dump([], f)
         return []
-
+print("Tong 2 so:")
 def tong():
     return
 def save_data(file_path, data):
