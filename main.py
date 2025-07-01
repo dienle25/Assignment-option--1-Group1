6179571f7bd05facd11fd219388c71e275e9fbcb
TranQuangHa <hatqca191679@gmail.com>
2025-07-01 20:09:07 +0700

    themtu196den214

--- main.py
+++ main.py
@@ -193,8 +193,25 @@ def export_to_csv():
 # ==============================================================================
 
 # --- Nhóm hàm xử lý giao diện cho Admin (Dành cho TV5) ---
-def tinh_tong(a,b):
-    return a + b
+def show_admin_menu():
+    while True:
+        print("\nAdmin Menu:")
+        print("1. Tạo tại khoảng")
+        print("2. Xem nội dung ")
+        print("3.Sửa đổi tài khoảng")
+        print("4. Quay lại menu chính ")
+        print("5. Thoát")
+        choice = input (" nhập lựa chọn của bạn:")
+        if choice == '1':
+            print("Tạo tài khoảng ")
+        elif choice =='2':
+            print("xem nội dung")
+        elif choice =='3':
+            print("Sửa đổi tài khoảng")
+        elif choice =='4':
+            print("Quay lại menu chính")
+        elif choice =='5':
+            print("Thoát")
 
 def handle_create_event():
     """(UI/UX - TV5) Xử lý luồng tạo sự kiện mới."""
